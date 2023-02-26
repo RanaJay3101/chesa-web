76192b124ffd6070347fe012e606cae9f7535769
RanaJay3101 <104300431+RanaJay3101@users.noreply.github.com>
2023-02-27 00:09:41 +0530

    Add files via upload

---
+++ src/routes/ContactUs/+page.svelte
@@ -0,0 +1,40 @@
+<section class="text-gray-600 body-font">
+	<div class="container px-5 py-24 mx-auto">
+		<div class="flex flex-col text-center w-full mb-20">
+			<h1 class="text-5xl  font-black title-font mb-4 text-gray-900"><ul>Contact Us</ul></h1>
+			<!-- <pre class="lg:w-2/3 mx-auto leading-relaxed text-base">
+Name: Chemical Engineering Students'Association
+Mail ID: chesa@nirmauni.ac.in 
+
+President: Shrey Kaneria
+Mail ID: 20BCH0061@nirmauni.ac.in
+Contact Number: 8866200617
+
+Vice President: Jayvir Atodariya
+Mail ID: 20BCH007@nirmauni.ac.in
+Contact Number: 7433834125
+			</pre> -->
+
+            <div class="text-left">
+                    <b>Name : </b>Chemical Engineering Students'Association <br>
+                    <b>Mail ID: </b>chesa@nirmauni.ac.in <br><br>
+                    
+
+                    <b>President:</b> Shrey Kaneria <br>
+<b>Mail ID: </b> 20BCH0061@nirmauni.ac.in <br>
+<b>Contact Number:</b> 8866200617 <br><br>
+
+<b>Vice President:</b> Jayvir Atodariya <br>
+<b>Mail ID: </b> 20BCH007@nirmauni.ac.in <br>
+<b>Contact Number:</b> 7433834125 <br><br>
+
+
+<div class=" text-center">
+    <a  class="text-red" href="https://technology.nirmauni.ac.in/student_work/chesa/">ChESA - Nirma University</a> 
+</div>
+
+                
+            </div>
+		</div>
+	</div>
+</section>
