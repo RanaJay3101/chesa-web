7b796dd10b4b98d06c0e101a8b666f10da0f5d5f
RanaJay3101 <104300431+RanaJay3101@users.noreply.github.com>
2023-02-26 16:03:04 +0530

    Update +page.svelte

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -3,24 +3,88 @@
 
 	const teamMembers = [
 		{
-			name: 'Raj Patel',
+			name: 'Shrey Kaneria',
 			image:
-				'https://images.unsplash.com/photo-1677241817906-a84211b4f627?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-			linkedIn: '',
-			position: 'Developer'
+				'/t1.webp',
+			position: 'President'
 		},
 		{
-			name: 'Raj Patel',
+			name: 'Jayvir Atodariya',
 			image:
-				'https://images.unsplash.com/photo-1677241817906-a84211b4f627?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
-			linkedIn: '',
-			position: 'Developer'
+				'/t2.jpg',
+			position: 'Vice President'
+		},
+		{
+			name: 'Manav Agrawal',
+			image:
+				'/t3.webp',
+			position: 'Treasurer'
+		},
+		{
+			name: 'Meet Shah',
+			image:
+				'/t4.webp',
+			position: 'General Secretary'
+		},
+		{
+			name: 'Ansh Singh',
+			image:
+				'/t5.webp',
+			position: 'Organizing Secretary'
+		},
+		{
+			name: 'Aryan Anand',
+			image:
+				'/t6.webp',
+			position: 'Editor-in-Chief'
+		},
+		{
+			name: 'Rishabh Singh',
+			image:
+				'/t7.webp',
+			position: 'Technical head'
+		},
+		{
+			name: 'Priyank thakkar',
+			image:
+				'/t8.webp',
+			position: 'Graphics Head'
+		},
+		{
+			name: 'Sanskar Jain',
+			image:
+				'/t9.webp',
+			position: 'Sponsorship Head'
+		},
+		{
+			name: 'Khushi Patel',
+			image:
+				'/t10.webp',
+			position: 'Creative Head'
+		},
+		{
+			name: 'Krishi Prajapati',
+			image:
+				'/t11.webp',
+			position: 'Creative Head'
+		},
+		{
+			name: 'Romit Patel',
+			image:
+				'/t12.webp',
+			position: 'Sports Head'
+		},
+		{
+			name: 'Bhargav Patel',
+			image:
+				'/t13.webp',
+			position: 'Sports Head'
 		}
 	];
 </script>
 
-<div class="max-w-[1440px] mx-auto py-8 px-6">
-	<h1 class="text-4xl font-black py-4 underline">Meet our team</h1>
+<div class="max-w-[1440px] mx-auto px-6">
+	<h1 class="text-4xl font-black py-8 underline text-center ">Meet our team</h1>
 	<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
 		{#each teamMembers as member}
 			<TeamCard {...member} />
