567f1e97fb87a07a32ab5f2085fe809d5cbdd52d
RanaJay3101 <104300431+RanaJay3101@users.noreply.github.com>
2023-03-03 16:03:03 +0530

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,36 +1,29 @@
-<footer class="p-4 bg-white md:px-6 md:py-8 max-w-[1440px] mx-auto">
+<footer class="container px-6 py-8 mx-auto shadow">
 	<div class="sm:flex sm:items-center sm:justify-between">
 		<a href="/" class="flex items-center mb-4 sm:mb-0">
-			<img
-				src="l2.png"
-				class="h-12 w-18 mr-3"
-				alt="Logo"
-			/>
-			<img
-				src="l1.png"
-				class="h-14 w-18 mr-3"
-				alt="Logo"
-			/>
+			<img src="/l1.png" class="h-20 w-25 mr-3" alt="Logo" />
 			<span class="self-center text-2xl font-semibold whitespace-nowrap">ChESA</span>
 		</a>
 		<ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0">
 			<li>
-				<a href="https://www.instagram.com/chesa_nirma_university/?igshid=OTJlNzQ0NWM%3D" class="mr-4 hover:underline md:mr-6 text-2xl hover:text-orange-400">
+				<a
+					href="https://www.instagram.com/chesa_nirma_university/?igshid=OTJlNzQ0NWM%3D"
+					class="mr-4 hover:underline md:mr-6 text-2xl hover:text-orange-400"
+				>
 					<i class="bx bxl-instagram" />
 				</a>
 			</li>
 			<li>
-				<a href="https://www.linkedin.com/company/c-h-e-s-a/" class="mr-4 hover:underline md:mr-6 text-2xl hover:text-orange-400">
+				<a
+					href="https://www.linkedin.com/company/c-h-e-s-a/"
+					class="mr-4 hover:underline md:mr-6 text-2xl hover:text-orange-400"
+				>
 					<i class="bx bxl-linkedin" />
-				</a>
-			</li>
-			<li>
-				<a href="/" class="mr-4 hover:underline md:mr-6 text-2xl hover:text-orange-400">
-					<i class="bx bxl-twitter" />
 				</a>
 			</li>
 		</ul>
 	</div>
-	<hr class="my-6 sm:mx-auto  lg:my-8" />
-	<span class="block text-sm text-gray-500 sm:text-center"> " ChESA believes that the future belongs to those who believe in the beuty of their dreams " </span>
+	<span class="block text-sm text-gray-500 sm:text-center">
+		ChESA believes that the future belongs to those who believe in the beuty of their dreams.
+	</span>
 </footer>
