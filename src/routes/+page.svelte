f237c07e3d0455da35fa68c216a8c6fc2893324b
RanaJay3101 <104300431+RanaJay3101@users.noreply.github.com>
2023-03-03 16:07:41 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,108 +1,15 @@
-<script>
-	import TeamCard from './team/TeamCard.svelte';
-</script>
-
-<!-- component -->
-<!-- This is an example component -->
-<div class="max-w-2xl mx-auto">
-	<div id="default-carousel" class="relative" data-carousel="static">
-		<!-- Carousel wrapper -->
-		<div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-			<!-- Item 1 -->
-			<div class=" duration-70 ease-in-out" data-carousel-item>
-				<span
-					class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800"
-					>First Slide</span>
-				<img
-					src="main.jpg"
-					class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-					alt="hi"
-				/>
-			</div>
-			<div class="duration-70 ease-in-out" data-carousel-item>
-				<span
-					class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800"
-					>Second Slide</span>
-				<img
-					src="main.jpg"
-					class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-					alt="hi"
-				/>
-			</div>
-		</div>
-		<!-- Slider indicators -->
-		<div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-			<button
-				type="button"
-				class="w-3 h-3 rounded-full"
-				aria-current="false"
-				aria-label="Slide 1"
-				data-carousel-slide-to="0"
-			/>
-			<button
-				type="button"
-				class="w-3 h-3 rounded-full"
-				aria-current="false"
-				aria-label="Slide 2"
-				data-carousel-slide-to="1"
-			/>
-			<button
-				type="button"
-				class="w-3 h-3 rounded-full"
-				aria-current="false"
-				aria-label="Slide 3"
-				data-carousel-slide-to="2"
-			/>
-		</div>
-		<!-- Slider controls -->
-		<button
-			type="button"
-			class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
-			data-carousel-prev
-		>
-			<span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-				<svg
-					class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-					fill="none"
-					stroke="currentColor"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M15 19l-7-7 7-7"
-					/></svg
-				>
-				<span class="hidden">Previous</span>
-			</span>
-		</button>
-		<button
-			type="button"
-			class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
-			data-carousel-next>
-			<span
-				class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-				<svg
-					class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-					fill="none"
-					stroke="currentColor"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M9 5l7 7-7 7"
-					/></svg
-				>
-				<span class="hidden">Next</span>
-			</span>
-		</button>
+<div class="max-w-5xl my-8 container px-4 mx-auto">
+	<div class=" bg-no-repeat bg-center bg-contain h-[24rem]" style="background-image: url('/main.png');">
+		<div class="flex items-center justify-center " />
 	</div>
-	
-	<p class="mt-5 text-center ">
-		Welcome to Aarambh - 2023 Organized by ChESA 
+
+	<h1 class="font-bold text-5xl text-center mt-12 pb-4">About AARAMBH</h1>
+	<p class="sm:text-center text-justify">
+		Whilst every college student has experienced the zest of Events in a Fest, AARAMBH is set to
+		elevate that experience to the next level. With the amalgamation of various technical and
+		cultural events, it will cater to the talents of students across all fields. The organising team
+		of ChESA hopes to bequeath a memorable experience upon all the participants and welcomes you all
+		to this two day Extravaganza!
 	</p>
-	<script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
+
 </div>
